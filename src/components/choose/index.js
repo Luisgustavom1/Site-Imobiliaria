0d423586d774c8b435135946a7cf67e6a010fb05
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-07-31 18:18:24 -0300

    Grid certo

--- src/components/choose/index.js
+++ src/components/choose/index.js
@@ -1,8 +1,8 @@
 import london from '../../assets/imgs/London.svg'
 import sydney from '../../assets/imgs/Sydney.svg'
-import newYork from '../../assets/imgs/NewYork.svg'
-import australia from '../../assets/imgs/Australia.svg'
-import tokyo from '../../assets/imgs/Tokyo.svg'
+import newYork from '../../assets/imgs/newYork.svg'
+import australia from '../../assets/imgs/australia.svg'
+import tokyo from '../../assets/imgs/tokyo.svg'
 
 import { ChooseStyle, CitysGrid } from './styled'
 export default function Choose(){
@@ -11,11 +11,11 @@ export default function Choose(){
             <h1>Choose your perfect city</h1>
             <p>finding the dream for every owner</p>
             <CitysGrid>
-                <img src={london} alt='Imagem cidade London'></img>
-                <img src={sydney} alt='Imagem cidade London'></img>
-                <img src={australia} alt='Imagem cidade London'></img>
-                <img src={newYork} alt='Imagem cidade London'></img>
-                <img src={tokyo} alt='Imagem cidade London'></img>
+                <div className='london'><img src={london} alt='Imagem cidade London'></img></div>
+                <div className='sydney'><img src={sydney} alt='Imagem cidade London'></img></div>
+                <div className='australia'><img src={australia} alt='Imagem cidade London'></img></div>
+                <div className='NY'><img src={newYork} alt='Imagem cidade London'></img></div>
+                <div className='tokyo'><img src={tokyo} alt='Imagem cidade London'></img></div>
             </CitysGrid>
         </ChooseStyle>
     )
--- src/components/choose/styled.js
+++ src/components/choose/styled.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import london from '../../assets/imgs/London.svg'
+import sydney from '../../assets/imgs/Sydney.svg'
+import newYork from '../../assets/imgs/newYork.svg'
+import australia from '../../assets/imgs/australia.svg'
+import tokyo from '../../assets/imgs/tokyo.svg'
 export const ChooseStyle = styled.section`
     margin-top: 119px;
     p{
@@ -13,19 +18,20 @@ export const CitysGrid = styled.div`
     grid-template-rows: 1fr 1fr;
     grid-template-areas: 'london NY australia'
                         'sydney NY tokyo';
-    &:nth-child(1){
+    .london{
         grid-area: london;
+        background-image
     }
-    &:nth-child(2){
+    .sydney{
         grid-area: sydney;
     }
-    &:nth-child(3){
+    .australia{
         grid-area: australia;
     }
-    &:nth-child(4){
+    .NY{
         grid-area: NY;
     }
-    &:nth-child(5){
+    .tokyo{
         grid-area: tokyo;
     }
 `
